2f63daa67656eff21f77268fe80a6b9486d73062
BruceChen <979527682@qq.com>
2019-05-11 10:49:39 +0800

    2019-05-11

---
+++ ImageClassification/ImagePrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ImageClassification
+{
+    // <SnippetDeclareTypes>
+    public class ImagePrediction
+    {
+        public float[] Score;
+
+        public string PredictedLabelValue;
+    }
+    // </SnippetDeclareTypes>
+}
